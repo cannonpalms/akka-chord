4da3367cbda025fdea9c89f7228b2761a462530c
Cannon Palms <cannonpalms@me.com>
2020-12-11 18:16:09 -0500

    add simulation tracker

--- modules/demo/src/main/scala/com/tristanpenman/chordial/demo/SimulationTracker.scala
+++ modules/demo/src/main/scala/com/tristanpenman/chordial/demo/SimulationTracker.scala
@@ -2,22 +2,26 @@ package com.tristanpenman.chordial.demo
 
 import akka.actor.{Actor, ActorLogging, Props}
 import com.tristanpenman.chordial.core.Event.{LookupCompleted, LookupStarted, NodeVisitedByLookup}
+import com.tristanpenman.chordial.demo.SimulationTracker.Lookup
 
 final class SimulationTracker extends Actor with ActorLogging {
 
-  private def receiveWhileReady(lookups: Map[Long, Int]): Receive = {
+  private def receiveWhileReady(lookups: Map[Long, Lookup]): Receive = {
     case LookupStarted(nodeId, queryId, lookupId) =>
-      context.become(receiveWhileReady(lookups + (lookupId -> 0)))
+      context.become(receiveWhileReady(lookups + (lookupId -> Lookup(nodeId, queryId, lookupId, 0))))
     case LookupCompleted(nodeId, queryId, successorId, lookupId) =>
-      val pathLength = lookups(lookupId)
-      log.info(s"""Lookup completed: { "nodeId": $nodeId, "queryId": $queryId, "successorId": $successorId } = $pathLength""")
+      val lookup = lookups(lookupId)
+      log.info(
+        s"""Lookup completed: { "nodeId": $nodeId, "queryId": $queryId, "successorId": $successorId, "pathLength": ${lookup.pathLength} }""")
     case NodeVisitedByLookup(nodeId, queryId, lookupId) =>
+      log.info(
+        s"""Node visited: { "lookupId": $lookupId, "nodeId": $nodeId, "queryId": $queryId }""")
       context.become(receiveWhileReady(lookups.updatedWith(lookupId)({
-        case Some(count) => Some(count + 1)
+        case Some(lookup) => Some(Lookup(lookup.nodeId, lookup.queryId, lookup.lookupId, lookup.pathLength + 1))
 
         // if we are receiving visit events before a lookup started event,
         // that should probably be a red flag, but oh well
-        case None => Some(1)
+        case None => Some(Lookup(nodeId, queryId, lookupId, 1))
       })))
   }
 
@@ -26,5 +30,7 @@ final class SimulationTracker extends Actor with ActorLogging {
 }
 
 object SimulationTracker {
+  final case class Lookup(nodeId: Long, queryId: Long, lookupId: Long, pathLength: Int)
+
   def props: Props = Props(new SimulationTracker)
 }
